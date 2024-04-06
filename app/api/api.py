9364e95deec68ec7e7bf7abd9d2d32dc492a9f66
Sachin Ramanathan <sachin.ramanathan123@gmail.com>
2024-04-06 13:41:12 -0700

    Fix issue with Flask connection

--- app/api/api.py
+++ app/api/api.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/get_something')
+@app.route('/api/get_something')
 def get_something():
+    return "Something"
+
+@app.route('/')
+def get_something1():
     return "Something"
